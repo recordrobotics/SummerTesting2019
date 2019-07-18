ea80007be30624f3d8920a7ffa39a5ca2e20e0fd
gavin@rohrer.org <gavin@rohrer.org>
2019-07-18 18:15:03 -0400

    added DriveTrain2018 for Monolith

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -23,4 +23,9 @@ public class RobotMap {
   // number and the module. For example you with a rangefinder:
   // public static int rangefinderPort = 1;
   // public static int rangefinderModule = 1;
+
+	public static int driveFrontRightPortOld = 2;
+	public static int driveFrontLeftPortOld = 0;
+	public static int driveBackRightPortOld = 3;
+	public static int driveBackLeftPortOld = 1;
 }
---
+++ src/main/java/frc/robot/subsystems/DriveTrain2018.java
@@ -0,0 +1,38 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * An example subsystem.  You can replace me with your own Subsystem.
+ */
+public class DriveTrain2018 extends DriveTrain {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+
+
+  public void initDefaultCommand(){};
+  //  {
+  //   // Set the default command for a subsystem here.
+  //   // setDefaultCommand(new MySpecialCommand());
+  // }
+
+  public boolean moveLeftWheels(double distance){
+    return true;
+  };
+  public boolean moveRightWheels(double distance){
+    return true;
+  };
+  public double getRightEncoder(){
+    return 0.0;
+  };
+  public double getLeftEncoder(){
+    return 0.0;
+  };
+}
