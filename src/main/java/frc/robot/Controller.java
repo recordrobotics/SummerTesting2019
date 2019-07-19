2e34f81e6a721ec74aaa13a64e3e28139c9cac13
gavin@rohrer.org <gavin@rohrer.org>
2019-07-19 11:40:51 -0400

    Not getting rid of Controller.java, wrote test implementation for Controller.java

--- src/main/java/frc/robot/Controller.java
+++ src/main/java/frc/robot/Controller.java
@@ -5,7 +5,27 @@ import frc.robot.RobotMap;
 
 
 public class Controller{
-	//TODO implement this class so it holds controller objects mapped to their colors / button names
-	// public Joystick buttonPanel = new Joystick(RobotMap.buttonPanelPort);
-	
+	public Joystick buttonPanel;
+	public JoystickButton greenleft;
+	public JoystickButton greenright;
+	public JoystickButton yellowleft;
+	public JoystickButton yellowright;
+	public JoystickButton redleft;
+	public JoystickButton redright;
+	public JoystickButton blueleft;
+	public JoystickButton blueright;
+	public JoystickButton white;
+
+	public Controller(){
+		buttonPanel = new Joystick(RobotMap.buttonPanelPort);
+		greenleft = new Joystick(RobotMap.greenleft);
+		greenright = new Joystick(RobotMap.greenright);
+		yellowleft = new Joystick(RobotMap.yellowleft);
+		yellowright = new Joystick(RobotMap.yellowright);
+		redleft = new Joystick(RobotMap.redleft);
+		redright = new Joystick(RobotMap.redright);
+		blueleft = new Joystick(RobotMap.blueleft);
+		blueright = new Joystick(RobotMap.blueright);
+		white = new Joystick(RobotMap.white);
+	}
 }
